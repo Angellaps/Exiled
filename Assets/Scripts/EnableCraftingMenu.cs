cca89690f51decf6e6e3b7ec698c62c67f1736a8
Aggelos <13-12795@saeinstitute.edu>
2021-01-18 13:47:57 +0200

    CraftingUI

---
+++ Assets/Scripts/EnableCraftingMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableCraftingMenu : MonoBehaviour
+{
+    public GameObject craftUI;
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            craftUI.SetActive(true);
+
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            craftUI.SetActive(false);
+
+        }
+    }
+}
